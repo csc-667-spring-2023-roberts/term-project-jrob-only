6f73f7cb497e4a106eb19baf69a6190a223e034a
John Roberts <roberts.john@gmail.com>
2023-03-13 20:48:32 -0700

    added static file handling

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import rootRoutes from "./routes/root.js";
 import createHttpError from "http-errors";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(".", "backend", "static")));
 app.use("/", rootRoutes);
 
 app.listen(PORT, () => {
