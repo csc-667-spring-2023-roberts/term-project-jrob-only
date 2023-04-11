58bab71b10139dfe3b89190ec130c0601134bbf1
John Roberts <roberts.john@gmail.com>
2023-04-10 20:41:09 -0700

    Added test routes, created prod DB and added postinstall migration instructions

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const homeRoutes = require("./routes/static/home.js");
 const gamesRoutes = require("./routes/static/games.js");
 const lobbyRoutes = require("./routes/static/lobby.js");
 const authenticationRoutes = require("./routes/static/authentication.js");
+const testRoutes = require("./routes/static/test.js");
 
 const app = express();
 app.use(morgan("dev"));
@@ -23,7 +24,7 @@ if (process.env.NODE_ENV === "development") {
   const connectLiveReload = require("connect-livereload");
 
   const liveReloadServer = livereload.createServer();
-  liveReloadServer.watch(path.join(__dirname, "backend", "static"));
+  liveReloadServer.watch(path.join(__dirname, "static"));
   liveReloadServer.server.once("connection", () => {
     setTimeout(() => {
       liveReloadServer.refresh("/");
@@ -35,15 +36,16 @@ if (process.env.NODE_ENV === "development") {
 
 const PORT = process.env.PORT || 3000;
 
-app.set("views", path.join(__dirname, "backend", "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "backend", "static")));
+app.use(express.static(path.join(__dirname, "static")));
 
 app.use("/", homeRoutes);
 app.use("/games", gamesRoutes);
 app.use("/lobby", lobbyRoutes);
 app.use("/authentication", authenticationRoutes);
+app.use("/test", testRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
