3f3ecc873eaca47a71c3ad8d29b92d103cac5357
John Roberts <roberts.john@gmail.com>
2023-04-17 19:17:57 -0700

    Created forms; placeholder routes for auth

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # term-project-jrob-only
 
 1. Implement authentication
+   - GET /login
+   - POST /login
+   - GET /register
+   - POST /register
+   - GET /logout
 2. Implement session management
 3. Implement basic chat (websockets)
--- backend/routes/static/authentication.js
+++ backend/routes/static/authentication.js
@@ -2,12 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/sign-up", (_request, response) => {
-  response.render("sign-up", { title: "Jrob's Term Project" });
+router.get("/register", (_request, response) => {
+  response.render("register", { title: "Jrob's Term Project" });
+});
+
+router.post("/register", (_request, response) => {
+  response.render("register", { title: "Jrob's Term Project" });
 });
 
 router.get("/login", (_request, response) => {
   response.render("login", { title: "Jrob's Term Project" });
+});
+
+router.get("/post", (_request, response) => {
+  response.render("login", { title: "Jrob's Term Project" });
+});
+
+router.get("/logout", (_request, response) => {
+  response.redirect("/");
 });
 
 module.exports = router;
